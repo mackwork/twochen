488b2d809f3417abb3a16abfedbcff794cd3c916
David <david@DaviddeMacBook-Pro.local>
2015-12-26 21:30:46 +0800

    初始化项目

---
+++ aaa/aaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaa
+//
+//  Created by David on 15/12/26.
+//  Copyright © 2015年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaa/aaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaa
+//
+//  Created by David on 15/12/26.
+//  Copyright © 2015年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
