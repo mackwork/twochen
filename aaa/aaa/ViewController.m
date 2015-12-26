2d93899538923e4f22e06476c547605d06b0733f
David <david@DaviddeMacBook-Pro.local>
2015-12-26 21:43:26 +0800

    修改

--- aaa/aaa/ViewController.m
+++ aaa/aaa/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"今天的工作完成了");
 }
 
 - (void)didReceiveMemoryWarning {
